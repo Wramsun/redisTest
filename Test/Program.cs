eff28cb75b4b5b7e400eea89006b6d6e936b12fd
洋 刘 <979539341@qq.com>
2018-11-21 10:24:41 +0800

    update

--- Test/Program.cs
+++ Test/Program.cs
@@ -15,21 +15,22 @@ namespace Test
             {
                 //获取Redis操作接口
                 IRedisClient Redis = RedisManager.GetClient();
-                ////放入内存
-                //Redis.Set<string>("LoginName", "962464");
-                //Redis.Set<string>("PassWord", "123456");
-                ////保存到硬盘
-                //Redis.Save();
-                ////释放内存
-                //Redis.Dispose();
+                ////放入内存 (存入字符串数据)
+                Redis.Set<string>("LoginName", "962464");
+                Redis.Set<string>("PassWord", "123456");
 
+                //存入集合
                 List<string> testList = new List<string>();
                 testList.Add("test1");
                 testList.Add("test2");
                 testList.Add("test3");
                 testList.Add("test4");
+                Redis.Set("testKey", testList);
+                //保存到硬盘
+                Redis.Save();
+                //释放内存
+                Redis.Dispose();
 
-                Redis.Set("testKey",testList);
                 Redis.Remove("myKey");
                 Console.WriteLine(Redis.Get<string>("test"));
                 Console.ReadKey();
